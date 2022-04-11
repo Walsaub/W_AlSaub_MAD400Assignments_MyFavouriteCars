59c39ed52bfdc933d10e3cee156c128328ddb5ae
Walsaub <wissam.alsaub01@stclairconnect.ca>
2022-04-10 21:31:28 -0400

    finished adding a message using MessageService

--- W_AlSaub_MyFavouriteCar/src/app/content-detail/content-detail.component.ts
+++ W_AlSaub_MyFavouriteCar/src/app/content-detail/content-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarService } from '../CarService/Car.service';
 import { Car } from '../helper-files/content-interface';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-content-detail',
@@ -11,7 +12,7 @@ import { Car } from '../helper-files/content-interface';
 export class ContentDetailComponent implements OnInit {
   id?: number;
   individualCar?: Car;
-  constructor(private route: ActivatedRoute, private carService: CarService) { }
+  constructor(private route: ActivatedRoute, private carService: CarService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -21,8 +22,10 @@ export class ContentDetailComponent implements OnInit {
       this.id = +(params.get('id') ?? "0");
       this.carService.getContentItem(this.id).subscribe((singleCar) => {
         this.individualCar = singleCar;
+        this.messageService.add(`The Car with the title: ${singleCar.title} and id: ${this.id} was retrived`);
       });
     });
   }
+
 
 }
